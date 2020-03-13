e7b983b080702547da6dfe278eb74981192d24db
Alan Marx <alanmarx@hotmail.com>
2020-03-13 00:55:22 +0000

    created a test file for my breed fetching function

--- breed_fetcher.js
+++ breed_fetcher.js
@@ -1,17 +1,29 @@
 const request = require('request');
 
+const fetchBreedDescription = (breedName, callback) => {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (err, request, body) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback ('The requested breed cannot be found', null);
+      } else {
+        callback(null, data[0].description);
+      }
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
+  
 
 
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + process.argv.slice(2).join(' '), (err, request, body) => {
-  if (err) {
-    console.log('Error:', err);
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log('The requested breed cannot be found');
-  } else {
-    console.log(data[0].description);
-  }
-});
+
+
+
+
+
 
---
+++ test/breedFetcherTest.js
@@ -0,0 +1,22 @@
+const { fetchBreedDescription } = require('../breed_fetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors. ";
+      assert.equal(expectedDesc, desc);
+      done();
+    });
+  });
+  it('returns a an error for an invalid breed, via callback', (done) => {
+    fetchBreedDescription('Blitz', (err, desc) => {
+      assert.equal(err, 'The requested breed cannot be found');
+      const expectedDesc = null;
+      assert.equal(expectedDesc, desc);
+      done();
+    });
+  });
+});
